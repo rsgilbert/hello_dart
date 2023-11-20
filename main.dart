f7e4d2de8d4904c37961859a8468b38cef5a3b5d
Gilbert Ssenyonjo <gssenyonjo@gmail.com>
2023-11-20 07:08:23 +0300

    Added variables and printed 20th century

--- main.dart
+++ main.dart
@@ -1,4 +1,19 @@
 
 void main() {
-  print("Hello, world!");
+  var name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+
+  var image = {
+    'tags': ['saturn'],
+    'url': 'https://google.com'
+  };
+
+  if(year >= 2001) {
+    print('21st century');
+  } else if(year >= 1901) {
+    print('20th century');
+  }
+
 }
