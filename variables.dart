5a703e472fc1db8dca5093d7195e87a2d725e88e
Gilbert Ssenyonjo <gssenyonjo@gmail.com>
2023-11-23 06:31:34 +0300

    Finished looked at variables

--- variables.dart
+++ variables.dart
@@ -1,18 +1,35 @@
-int calcLineCount() {
-  print('Calculating line count');
-  return 10;
-}
-
-late int lineCount = calcLineCount();
 
 void main() {
-  String? name = 'Bob';
-  pr(name);
+  var d = new Dig();
+  print(Dig.v);
+  print(d.m);
 
-  // print(lineCount);
-  print('hi');
+  const bar  = 100;
+  const double atm = 1.01 * bar;
+  print(bar);
+  print(atm);
+
+  var abc =  [];
+  // xyz = [3];
+  abc.add(9);
+  print(abc);
+
+  const Object i = 3;
+  const list = [i as int, 5];
+  final map = { if (i is int) i: 'int'};
+  const set = { if (list is List<int>) ...list };
+
+  print(i);
+  print(list);
+  print(map);
+  print(set);
+
+  map[3] = 'double';
+  print(map);
 }
 
-void pr(String nm) {
-  print(nm.length);
+
+class Dig {
+  var m = 3;
+  static const v = 4;
 }
